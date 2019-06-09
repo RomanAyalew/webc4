0adb1fe94af67962818150c4ec435e396801890d
RomanAyalew <50527448+RomanAyalew@users.noreply.github.com>
2019-06-09 14:01:44 -0700

    current weather component

--- local-weather-app/src/app/app.component.ts
+++ local-weather-app/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class currentwaethercAppComponent {
+export class AppComponent {
   title = 'local-weather-app';
-current:}
+//current: 
+}
